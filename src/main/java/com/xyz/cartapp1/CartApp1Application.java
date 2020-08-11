a6c3e6593b3312894a67c449b6994e8f18aa6e52
Aditi <aditiparashar95@gmail.com>
2020-08-11 16:34:32 +0530

    Backend initial commit

---
+++ src/main/java/com/xyz/cartapp1/CartApp1Application.java
@@ -0,0 +1,13 @@
+package com.xyz.cartapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartApp1Application.class, args);
+	}
+
+}
